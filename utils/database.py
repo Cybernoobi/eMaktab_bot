8a6d6b59ff869428a48a70421a306174981f83ae
Cybernoob <79698178+Cybernoobi@users.noreply.github.com>
2024-07-02 21:29:38 +0500

    Added description

--- utils/database.py
+++ utils/database.py
@@ -4,17 +4,35 @@ from bs4 import BeautifulSoup
 
 
 async def database_connect(db_name: str):
+    """
+    Connection for sqlite3 database
+
+    :param db_name: Write database name
+    :return: sqlite connect and sqlite cursor
+    """
     connection = await aiosqlite.connect(db_name)
     cursor = await connection.cursor()
     return connection, cursor
 
 
 async def commit_and_close(connection: aiosqlite.Connection):
+    """
+    Commit and close database
+
+    :param connection: connect for database_connect
+    :return: Noting
+    """
     await connection.commit()
     await connection.close()
 
 
 async def create_tables(db_name: str):
+    """
+    Create a tables for database
+
+    :param db_name: Write database name
+    :return: Nothing
+    """
     conn, cursor = await database_connect(db_name)
     sql = '''
         DROP TABLE IF EXISTS tg_users;
@@ -39,6 +57,12 @@ async def create_tables(db_name: str):
 
 
 async def check_user_tg(db_name: str, user_id: int):
+    """
+    Checking if there is a user_id in the database
+
+    :param db_name: Write database name
+    :param user_id: Write the telegram user id
+    """
     conn, cursor = await database_connect(db_name)
     await cursor.execute('SELECT * FROM tg_users WHERE user_id = ?;', (user_id,))
     user = await cursor.fetchone()
@@ -46,6 +70,12 @@ async def check_user_tg(db_name: str, user_id: int):
 
 
 async def check_user_emaktab(db_name: str, login: str):
+    """
+    Checking if there is a login in the database
+
+    :param db_name: Write database name
+    :param user_id: Write the telegram user id
+    """
     conn, cursor = await database_connect(db_name)
     await cursor.execute('SELECT * FROM emaktab WHERE login = ?;', (login,))
     account = await cursor.fetchone()
@@ -53,6 +83,16 @@ async def check_user_emaktab(db_name: str, login: str):
 
 
 async def add_tg_user(db_name: str, user_id: int, username: str, full_name: str):
+    """
+    Adding a user's telegram to the database
+
+    :param db_name: Write database name
+    :param user_id: Write telegram user id
+    :param username: Write telegram username (@)
+    :param full_name: Write telegram full name
+
+    :return: Nothing
+    """
     conn, cursor = await database_connect(db_name)
     registration_date = datetime.now().strftime("%m.%d.%Y %H:%M:%S")
     await cursor.execute('INSERT INTO tg_users(user_id, username, full_name, registration_date) VALUES (?, ?, ?, ?);',
@@ -69,18 +109,44 @@ New user:
 
 
 async def add_emaktab(db_name: str, user_id: int, login: str, password: str):
+    """
+    Adding a user's eMaktab to the database
+
+    :param db_name: Write database name
+    :param user_id: Write telegram user id
+    :param login: Write eMaktab login
+    :param password: Write eMaktab password
+
+    :return: Nothing
+    """
     conn, cursor = await database_connect(db_name)
     await cursor.execute('INSERT INTO emaktab(user_id, login, password) VALUES (?, ?, ?);', (user_id, login, password))
     await commit_and_close(conn)
 
 
 async def delete_tg_user(db_name: str, user_id: int):
+    """
+    Delete telegram user for database
+
+    :param db_name: Write database name
+    :param user_id: Write telegram user id
+
+    :return: Noting
+    """
     conn, cursor = await database_connect(db_name)
     await cursor.execute('DELETE FROM tg_users WHERE user_id = ?;', (user_id,))
     await commit_and_close(conn)
 
 
 async def delete_emaktab_login(db_name: str, user_id: int):
+    """
+    Delete eMaktab user for database
+
+    :param db_name: Write database name
+    :param user_id: Write telegram user id
+
+    :return: Noting
+    """
     conn, cursor = await database_connect(db_name)
     await cursor.execute('DELETE FROM emaktab WHERE user_id = ?;', (user_id,))
     await commit_and_close(conn)
