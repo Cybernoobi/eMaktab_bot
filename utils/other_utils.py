6623f1f93df8c7daefce9b6fb61b9da84d339084
Cybernoob <ibrasamandar.2009@gmail.com>
2024-09-24 22:50:58 +0500

    Edited cookies work

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -3,14 +3,14 @@ import json
 
 class CookiesManager:
     def __init__(self, login: str, key: dict = None):
-        self.key = key
         self.login = login
+        self.key = key
 
-    def save(self):
+    def save_cookie(self):
         if self.key is not None:
             with open(f'./utils/cookies/{self.login}.json', mode='w') as file:
                 json.dump(self.key, file, indent=4)
 
-    def get_cookie(self) -> list:
+    def get_cookie(self) -> dict:
         with open(f'./utils/cookies/{self.login}.json', mode='r') as file:
             return json.load(file)
