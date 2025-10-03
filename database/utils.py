9b00e42616750dcbdabbe2ad06010694766ca357
Cybernoob <ibrasamandar.2009@gmail.com>
2025-10-03 21:00:14 +0500

    Create utils.py

---
+++ database/utils.py
@@ -0,0 +1,51 @@
+from typing import Literal
+
+from sqlalchemy import select
+
+from utils.enums import LocalizationLiteral
+from .models import TelegramUser, eMaktabUser, async_session
+
+
+async def add_tg_user(user_id: int, username: str | None, full_name: str, localization: LocalizationLiteral) -> None:
+    """
+    Adds a new user to the database if they do not already exist.
+
+    :param user_id: The unique identifier for the user in Telegram.
+    :param username: The username of the user in Telegram.
+    :param full_name: The full name of the user in Telegram.
+    :param localization: The localization of the user in Telegram.
+
+    :return: None
+    """
+    async with async_session() as session:
+        user = await session.scalar(select(TelegramUser).where(TelegramUser.telegram_id == user_id))
+
+        if not user:
+            session.add(TelegramUser(telegram_id=user_id,
+                                     username=username,
+                                     full_name=full_name,
+                                     localization=localization))
+            await session.commit()
+
+
+async def add_em_user(user_id: int, login: str, password: str, **kwargs) -> None:
+    async with async_session() as session:
+        user = await session.scalar(select(TelegramUser).where(eMaktabUser.telegram_id == user_id))
+
+        if not user:
+            session.add(eMaktabUser(telegram_id=user_id,
+                                    login=login,
+                                    password=password))
+            await session.commit()
+
+
+async def get_user_for_tg_id(user_id: int, fileter: Literal['tg', 'em']) -> TelegramUser | eMaktabUser | None:
+    async with async_session() as session:
+        user = None
+        if fileter == 'tg':
+            user = await session.scalar(select(TelegramUser).where(TelegramUser.telegram_id == user_id))
+
+        elif fileter == 'em':
+            user = await session.scalar(select(eMaktabUser).where(eMaktabUser.telegram_id == user_id))
+
+        return user
