15e110f303b283f13ce98705d228a77dfb9dabb5
Cybernoob <ibrasamandar.2009@gmail.com>
2025-10-03 20:56:24 +0500

    update for localization

--- dispatcher.py
+++ dispatcher.py
@@ -1,15 +1,12 @@
 from aiogram import Dispatcher
 
-from fluent_loader import get_fluent_localization
+from fluent_loader import L10N_MAPPING
 from middlewares import L10nMiddleware
-
-# init locale
-locale = get_fluent_localization()
 
 # init dispatcher
 dp = Dispatcher()
 
 # Apply middlewares
-dp.message.outer_middleware(L10nMiddleware(locale))
-dp.pre_checkout_query.outer_middleware(L10nMiddleware(locale))
-dp.callback_query.outer_middleware(L10nMiddleware(locale))
+dp.message.outer_middleware(L10nMiddleware(l10n_mapping=L10N_MAPPING))
+dp.pre_checkout_query.outer_middleware(L10nMiddleware(l10n_mapping=L10N_MAPPING))
+dp.callback_query.outer_middleware(L10nMiddleware(l10n_mapping=L10N_MAPPING))
