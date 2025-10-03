628e054380ca4d4f6bbbee9a61a4799a3782851e
Cybernoob <ibrasamandar.2009@gmail.com>
2025-10-03 20:55:34 +0500

    bug fixed

--- database/models.py
+++ database/models.py
@@ -5,10 +5,10 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from config_reader import get_config, DBConfig
 
-DATABASE_URL: DBConfig = get_config(model=DBConfig, root_key="database_url")
+DATABASE_URL: DBConfig = get_config(model=DBConfig, root_key="database")
 
 engine = create_async_engine(DATABASE_URL.uri.get_secret_value(), echo=True)
-SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -18,20 +18,20 @@ class TelegramUser(Base):
     __tablename__ = "telegram_users"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
-    telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
     full_name: Mapped[str] = mapped_column(String, nullable=False)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=True)
-    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False, default=datetime.timezone.utc)
+    localization: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=False, default=lambda: datetime.datetime.now(datetime.timezone.utc))
 
 
 class eMaktabUser(Base):
     __tablename__ = "emaktab_users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    telegram_id: Mapped[int] = mapped_column(BigInteger, ForeignKey(TelegramUser.telegram_id), nullable=False)
-    auth_token: Mapped[str] = mapped_column(String(350), nullable=False)
-    person_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
-    school_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    login: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, ForeignKey(TelegramUser.telegram_id, ondelete="CASCADE"), nullable=False)
 
 async def init_db():
     async with engine.begin() as conn:
